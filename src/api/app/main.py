ed33e9b8a0cac491fbe20fac830f9fc981a04363
Vincent Leclerc <vleclerc@inow.fr>
2026-02-26 17:08:07 +0100

    add cors

--- src/api/app/main.py
+++ src/api/app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 from .routes_jobs import router as jobs_router
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = ["*"]
 
 app = FastAPI(title="Doc processing API", description="API de génération de documents", version="1.0.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,      # domaines autorisés
+    allow_credentials=False,
+    allow_methods=["*"],        # GET, POST, PUT, DELETE...
+    allow_headers=["*"],
+)
 
 app.include_router(jobs_router)
 
