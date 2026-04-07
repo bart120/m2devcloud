12bf7471aace6859232c98599b2c97bbf579ae0e
Vincent Leclerc <vleclerc@inow.fr>
2026-04-07 17:12:41 +0200

    test

--- src/functions/worker/function_app.py
+++ src/functions/worker/function_app.py
@@ -5,7 +5,7 @@ app = func.FunctionApp()
 
 @app.blob_trigger(arg_name="myblob", path="doc-storage/input/{name}",
                                connection="docstorageprof_STORAGE") 
-def WorkerFile(myblob: func.InputStream):
+def Test(myblob: func.InputStream):
     logging.info(f"Version CI/CD => Python blob trigger function processed blob"
                 f"Name: {myblob.name}"
                 f"Blob Size: {myblob.length} bytes")
