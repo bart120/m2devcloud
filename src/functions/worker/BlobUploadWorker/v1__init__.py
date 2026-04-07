19c806a6681a0653dedb9ff5788a96f91db2c9ee
Vincent Leclerc <vleclerc@inow.fr>
2026-04-07 16:55:42 +0200

    add function azure worker

---
+++ src/functions/worker/BlobUploadWorker/v1__init__.py
@@ -0,0 +1,6 @@
+import logging
+
+def main(myblob: func.InputStream):
+    logging.info(f"Python blob trigger function processed blob"
+                f"Name: {myblob.name}"
+                f"Blob Size: {myblob.length} bytes")
---
+++ src/functions/worker/function_app.py
@@ -0,0 +1,11 @@
+import azure.functions as func
+import logging
+
+app = func.FunctionApp()
+
+@app.blob_trigger(arg_name="myblob", path="doc-storage/input/{name}",
+                               connection="docstorageprof_STORAGE") 
+def WorkerFile(myblob: func.InputStream):
+    logging.info(f"Version CI/CD => Python blob trigger function processed blob"
+                f"Name: {myblob.name}"
+                f"Blob Size: {myblob.length} bytes")
